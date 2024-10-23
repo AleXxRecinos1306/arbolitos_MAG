76b6882d226d76a08d461287a6f8f23b8ed5d608
AleXxRecinos1306 <00379423@uca.edu.sv>
2024-10-22 19:43:58 -0600

    nueva funcion

--- arbol.h
+++ arbol.h
@@ -3,13 +3,29 @@
 
 #include <iostream>
 
-struct arbol {
+//se crea la estructura
+struct Arbol {
     int dato;
-    arbol *anterior=nullptr;
-    arbol *siguiente=nullptr;
+    Arbol *anterior=nullptr;
+    Arbol *siguiente=nullptr;
+
+    //con ayuda del constructor se crea un nuevo arbol
+    Arbol(int valor) : dato(valor) {}
 };
-    
-void Editar(struct &arbol);
+
+bool Modificar(Arbol* nodo, int valorAntiguo, int valorNuevo) {
+    // Valor no encontrado
+    if (nodo == nullptr) {
+        return false; 
+    }
+    // si coiciden se modifica el valor antiguo
+    if (nodo->dato == valorAntiguo) {
+        nodo->dato = valorNuevo; 
+        return true;
+    }
+    // recursividad con modificar para buscar en arbol anterior y sigguiente
+    return Modificar(nodo->anterior, valorAntiguo, valorNuevo) || Modificar(nodo->siguiente, valorAntiguo, valorNuevo);
+}
 
 
 #endif
