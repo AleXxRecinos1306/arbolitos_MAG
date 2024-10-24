e5f6f5d17902a7ec1cc1bbb02c66caa9b77607e3
reneesalmeron <00142524@uca.edu.sv>
2024-10-23 19:40:10 -0600

    Se agrego funcion Insertar y Buscar

--- arbol.h
+++ arbol.h
@@ -3,29 +3,58 @@
 
 #include <iostream>
 
-//se crea la estructura
+// se crea la estructura
 struct Arbol {
-    int dato;
-    Arbol *izquierda=nullptr;
-    Arbol *derecha=nullptr;
+  int dato;
+  Arbol *izquierda = nullptr;
+  Arbol *derecha = nullptr;
 
-    //con ayuda del constructor se crea un nuevo arbol
-    Arbol(int valor) : dato(valor) {}
+  // con ayuda del constructor se crea un nuevo arbol
+  Arbol(int valor) : dato(valor) {}
 };
 
-bool Modificar(Arbol* arbol, int valorAntiguo, int valorNuevo) {
-    // Valor no encontrado
-    if (arbol == nullptr) {
-        return false; 
+void Insertar(Arbol *&arbol, int n) {
+  // No existe nada
+  if (arbol == nullptr) {
+    arbol = new Arbol(n);
+    // Si existe
+  } else {
+    int valorRaiz = arbol->dato;
+    // Y el valor es menor al anterior se inserta a la izquierda
+    if (n < valorRaiz) {
+      Insertar(arbol->izquierda, n);
+      // Si es mayor se inserta a la derecha
+    } else {
+      Insertar(arbol->derecha, n);
     }
-    // si coiciden se modifica el valor antiguo
-    if (arbol->dato == valorAntiguo) {
-        arbol->dato = valorNuevo; 
-        return true;
-    }
-    // recursividad con modificar para buscar en arbol anterior y sigguiente
-    return Modificar(arbol->izquierda, valorAntiguo, valorNuevo) || Modificar(arbol->derecha, valorAntiguo, valorNuevo);
+  }
 }
 
+bool Buscar(Arbol *arbol, int n) {
+  if (arbol == nullptr) {
+    return false;
+  } else if (arbol->dato == n) {
+    return true;
+  } else if (n < arbol->dato) {
+    return Buscar(arbol->izquierda, n);
+  } else {
+    return Buscar(arbol->derecha, n);
+  }
+}
+
+bool Modificar(Arbol *arbol, int valorAntiguo, int valorNuevo) {
+  // Valor no encontrado
+  if (arbol == nullptr) {
+    return false;
+  }
+  // si coiciden se modifica el valor antiguo
+  if (arbol->dato == valorAntiguo) {
+    arbol->dato = valorNuevo;
+    return true;
+  }
+  // recursividad con modificar para buscar en arbol anterior y siguiente
+  return Modificar(arbol->izquierda, valorAntiguo, valorNuevo) ||
+         Modificar(arbol->derecha, valorAntiguo, valorNuevo);
+}
 
 #endif
